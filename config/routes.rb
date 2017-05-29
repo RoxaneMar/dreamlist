73f235af79027586270c27d01e92d6248bc4adc1
Roxane Marcilhacy <roxane.marcilhacy@gmail.com>
2017-05-29 14:04:31 +0200

    attachinary set up et user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/show'
+
+  get 'users/profile_edit'
+
+  get 'users/profile_update'
+
+  mount Attachinary::Engine => "/attachinary"
   devise_for :users
   resources :dreams
   root to: 'pages#home'
