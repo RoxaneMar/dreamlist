1bbc00cfa3e88209db5937a952f6506997f67325
Valentine Langer <valentine.langer@gmail.com>
2017-06-01 11:25:05 +0200

    contributor no nil

--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -2,5 +2,5 @@ class Contributor < ApplicationRecord
   belongs_to :user
   belongs_to :kitty
 
-  validates :amount, presence: true
+  validates :amount, presence: true, numericality: { greater_than: 0 }
 end
