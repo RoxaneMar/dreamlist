237b0d2df65f39186bf7b3110d835377649b2e9a
Valentine Langer <valentine.langer@gmail.com>
2017-06-05 12:55:26 +0200

    private and public contributions starting to be visible

--- app/models/kitty.rb
+++ app/models/kitty.rb
@@ -2,16 +2,7 @@ class Kitty < ApplicationRecord
   belongs_to :dream
   has_many :contributors
 
-  # def total_contribution
-  #   # contributors.sum(:amount)
-  #   contribution = 0
-  #   self.contributors.each do |contributor|
-  #     contribution += contributor.amount
-  #   end
-  #   contribution.to_i
-  # end
-
-  def total_contribution
+  def total_contribution_public
     contribution = 0
     self.contributors.each do |contributor|
       if contributor.private
@@ -23,9 +14,51 @@ class Kitty < ApplicationRecord
     contribution.to_i
   end
 
-  def contribution_avancement
+  def total_contribution_private
+    contribution = 0
+    self.contributors.each do |contributor|
+      if !contributor.private
+        contribution
+      else
+        contribution += contributor.amount
+      end
+    end
+    contribution.to_i
+  end
+
+  # for green progress bar
+  def contribution_avancement_public
+    if total_contribution_public >= goal_amount
+      100
+    elsif goal_amount > 0
+      (total_contribution_public.to_f / goal_amount)*100
+    else
+      100
+    end
+  end
+
+  def contribution_avancement_private
+    if total_contribution_private >= goal_amount
+      100
+    elsif goal_amount > 0
+      (total_contribution_private.to_f / goal_amount)*100
+    else
+      100
+    end
+  end
+
+  # for % of money pot reached
+  def real_contribution_avancement
     if goal_amount > 0
-      (total_contribution.to_f / goal_amount)*100
+      (total_contribution_public.to_f / goal_amount)*100
+    else
+      100
+    end
+  end
+
+  def real_contribution_avancement_private
+    if goal_amount > 0
+      (total_contribution_private.to_f / goal_amount)*100
     else
       100
     end
